3f3b8984fe8c8a318d9278eaf99889706f2b06d8
rafliaf <ahmad.farizqi23@gmail.com>
2022-11-30 15:27:04 +0700

    melakukan pull

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+
+    public float moveSpeed = 8f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+}
